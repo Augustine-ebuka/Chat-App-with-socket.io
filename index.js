af9820cf532f0cd692d3785d87a0c0f8d9846d7c
Hebrus <hebrusgraphics@gmail.com>
2023-04-02 01:29:09 +0100

    user tying features

--- index.js
+++ index.js
@@ -9,27 +9,44 @@ const path = require('path');
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'index.html'));
 });
-var numConnected  = 0
-io.on('connection', (socket) => {
-  numConnected += 1
-  socket.on('new user', user_id=>{
-    console.log(user_id);
-  })
-  console.log('connected',numConnected);
-   // Broadcast a message to all connected clients except the one that triggered the event
-  socket.broadcast.emit('connection', 'a user connected',numConnected);
 
-  // for disconnection
+var numConnected = 0
+
+// starting the socket here
+io.on('connection', (socket) => {
+
+  // count the number of connections
+  numConnected += 1
+
+  // Broadcast a message to all connected clients except the one that triggered the event
+  socket.broadcast.emit('user-connection', 'a user connected',numConnected);
+  
+  // for disconnection logic
   socket.on('disconnect', () => {
     numConnected--
     console.log('A user disconnected', numConnected);
     // Broadcast a message to all connected clients except the one that triggered the event
     socket.broadcast.emit('user-disconnected', 'A user has disconnected',numConnected);
   });
+  
+  // collecting new user event
+  socket.on('new user', user_id => {
+    socket.broadcast.emit('new user', user_id)
+    console.log(user_id);
+  })
+
+  // messages event
   socket.on('chat message',(id,msg )=> {
-    console.log(id,msg);
+    console.log(`${id}: `,msg);
     io.emit('chat message', id,msg);
   });
+
+// collecting user typing
+  socket.on('typingMessage', id => { 
+    console.log(`user ${id}...`);
+    io.emit('typing', id)
+  })
+  console.log('total user connected: ', numConnected);
 });
 
 http.listen(port, () => {
